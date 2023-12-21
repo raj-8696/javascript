1daa2ede7325cd8d9e0c724ee7ea4d643cc9fc7a
Rajnarayan <70699017+raj-8696@users.noreply.github.com>
2023-12-21 16:11:00 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accoundId = 5455;
+let accountEmail = "example@gmail.com";
+var accountPassword = "545512";
+accountCity = "Bihar";
+let accoundState;
+
+
+// accoundId = 2; not allowed becouse constent variable can not be change
+
+// console.log(accoundId);
+
+console.table([accoundId, accountEmail, accountPassword, accountCity, accoundState]);
+
+/**
+ * Prefer not to use var
+ * becouse issue in block scope and functional scope
+ */
+
