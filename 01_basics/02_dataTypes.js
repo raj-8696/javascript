3ff1db9237a031e3eb501acadf808dbcc9d9e16f
Rajnarayan <70699017+raj-8696@users.noreply.github.com>
2023-12-21 16:32:05 +0000

    datatypes discuss

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all js code as newer version
+
+// alert("Hello alret"); // we are using node js, not browser
+
+console.log(3 + 
+    
+    3); // code readibility shuld be high
+    
+console.log("Rajnish");
+
+
